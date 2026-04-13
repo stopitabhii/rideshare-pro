d8031b00fc7257948f9f158dd4ef0aee42c38e2c
Abhishek Tripathi <shutupabhi@outlook.com>
2026-04-13 19:27:14 +0530

    fix cors for production + local

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -17,10 +17,13 @@ export default function Signup() {
     e.preventDefault();
 
     try {
+      console.log(form);
+      
       const res = await API.post("/auth/register", form);
       localStorage.setItem("token", res.data.token);
       alert("Signup successful");
     } catch (err) {
+        console.log(err.response?.data);
       setError(err.response?.data?.error || err.message);
     }
   };
@@ -28,6 +31,13 @@ export default function Signup() {
   return (
     <div>
       <input placeholder="Name" onChange={e => setForm({...form, name: e.target.value})}/>
+      <input placeholder="Phone" onChange={e => setForm({...form, phone: e.target.value})}/>
+      <input placeholder="Organization" onChange={e => setForm({...form, organization: e.target.value})}/>
+      <select onChange={e => setForm({...form, role: e.target.value})}>
+      <option value="rider">Rider</option>
+      <option value="driver">Driver</option>
+      </select>
+
       <input placeholder="Email" onChange={e => setForm({...form, email: e.target.value})}/>
       <input placeholder="Password" onChange={e => setForm({...form, password: e.target.value})}/>
 
